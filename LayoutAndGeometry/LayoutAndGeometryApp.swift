c731d14675320d584d8af61db64a36e937460bff
Evgenii Shatokhin <shatokhin.e@northeastern.edu>
2022-05-05 17:39:39 +0400

    making a spinning hilux view of the list of views

---
+++ LayoutAndGeometry/LayoutAndGeometryApp.swift
@@ -0,0 +1,17 @@
+//
+//  LayoutAndGeometryApp.swift
+//  LayoutAndGeometry
+//
+//  Created by Evi St on 5/4/22.
+//
+
+import SwiftUI
+
+@main
+struct LayoutAndGeometryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
